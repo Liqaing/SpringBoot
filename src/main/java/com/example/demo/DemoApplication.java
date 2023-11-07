75d12fcfe18b9dfe64a45e4dff690d6b6f8aecd0
Rithy <huotrithy01@gmail.com>
2023-11-07 23:22:45 +0700

    Create student class, return student at API endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> student() {
+		return List.of(
+				new Student(
+						1,
+						"Rick",
+						19,
+						"r@gmail.com",
+						LocalDate.of(2004, Month.AUGUST, 19)
+				)
+		);
 	}
 }
---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,71 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private int id;
+    private String name;
+    private Integer age;
+    private String email;
+    private LocalDate dob;
+
+    // Constructor
+    public Student() {
+    }
+
+    public Student(int id, String name, Integer age, String email, LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Student(String name, Integer age, String email, LocalDate dob) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    // Setter and getter
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
