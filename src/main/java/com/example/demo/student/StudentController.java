8b3f698463aafaa0ab085ee6ad08fc222e31968f
Rithy <huotrithy01@gmail.com>
2023-11-08 22:33:18 +0700

    Create service layer, used for handle logic and talke to data layer. Implement Dependencies injection to inject dependencies (class) of a class into constructor without needing to create obj ot dependencies class internally

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,29 +1,29 @@
 /* This is an api layer
-    a controller class to control api request and student class
+    a controller class to control api request and talk to student service
 */
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    // Instant field store StudentService class reference
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = new StudentService();
+    }
+
     @GetMapping
     public List<Student> student() {
-        return List.of(
-                new Student(
-                        1,
-                        "Rick",
-                        19,
-                        "r@gmail.com",
-                        LocalDate.of(2004, Month.AUGUST, 19)
-                )
-        );
+        return this.studentService.getStudent();
     }
 }
